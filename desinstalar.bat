2a69481d676aa7e4f4ea39774c0681244ed15e96
Guilherme <maxguilham@protonmail.com>
2024-10-24 15:25:37 -0300

    Add files via upload

--- desinstalar.bat
+++ desinstalar.bat
@@ -1,1 +1,6 @@
+@echo off
 schtasks /delete /tn "ScriptInicializacao" /f
+if %ERRORLEVEL% NEQ 0 (
+    echo Falha ao tirar o script.
+    pause
+)
--- src.py
+++ src.py
@@ -1,9 +1,21 @@
 import ctypes
 import os
+import requests
+
+def check():
+    response = requests.get(url="https://raw.githubusercontent.com/mdsmax/fefipefefotrolagem/refs/heads/main/working.txt")
+    if "working" in response.text:
+        return True
+    else:
+        return False
 
 def set_wallpaper(image_path):
-    abs_path = os.path.abspath(image_path)
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 3)
+    working = check()
+    if working == True:
+        abs_path = os.path.abspath(image_path)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 3)
+    else:
+        exit()
 
 
 set_wallpaper("C:/Windows/System32/macaco.jpg")
